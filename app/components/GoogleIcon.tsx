d8fabd95394f1855e8989354fdac0dc900bcd402
winshine0326 <shane10340@gmail.com>
2025-10-05 21:04:58 +0900

    feat :: 구글 아이콘 svg로 변경

--- app/components/GoogleIcon.tsx
+++ app/components/GoogleIcon.tsx
@@ -1,24 +1,18 @@
 import styled from "@emotion/styled";
+import Image from "next/image";
 
 const IconWrapper = styled.div`
   width: 20px;
   height: 20px;
-  background: linear-gradient(
-    135deg,
-    #4285f4 0%,
-    #34a853 50%,
-    #fbbc05 75%,
-    #ea4335 100%
-  );
-  border-radius: 4px;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-weight: bold;
-  font-size: 14px;
-  color: white;
 `;
 
 export default function GoogleIcon() {
-  return <IconWrapper>G</IconWrapper>;
+  return (
+    <IconWrapper>
+      <Image src="/icons/google.svg" alt="Google" width={20} height={20} />
+    </IconWrapper>
+  );
 }

4cf7223ce8b73f122d55bf90bd116b56393dd9eb
winshine0326 <shane10340@gmail.com>
2025-10-05 17:23:46 +0900

    chore :: 플로팅 이미지 스크롤 방지

--- app/landing/style.ts
+++ app/landing/style.ts
@@ -123,8 +123,8 @@ export const CTAButtonsContainer = styled.div`
   gap: 24px;
   align-items: center;
   position: relative;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   flex-shrink: 0;
+  background: inherit;
 `;
 
 export const PrimaryCTA = styled.button`
@@ -231,6 +231,10 @@ export const FloatingImage = styled.img`
   max-width: none;
   width: 100%;
   height: 100%;
+  user-select: none;
+  -webkit-user-drag: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
 `;
 
 export const FeaturesSection = styled.div`

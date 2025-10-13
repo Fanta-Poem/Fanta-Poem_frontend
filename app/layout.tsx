098fa176c9d6350d3847b9353479837c98aa2e8b
winshine0326 <shane10340@gmail.com>
2025-10-13 19:56:55 +0900

    chore :: web폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./providers/query-provider";
 import AuthSessionProvider from "./providers/session-provider";
 import EmotionRegistry from "./lib/emotion";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "판타시",
@@ -29,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ko">
+      <body>
         <EmotionRegistry>
           <AuthSessionProvider>
             <QueryProvider>{children}</QueryProvider>
